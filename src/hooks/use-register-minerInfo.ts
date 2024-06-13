8431e57f83c263c74c4d6fa98a8859eb6e56e85e
chenmoonmo <chenmoonmo@gmail.com>
2024-06-13 15:10:21 +0800

    feat: waitForTransactionReceipt

--- src/hooks/use-register-minerInfo.ts
+++ src/hooks/use-register-minerInfo.ts
@@ -1,34 +1,19 @@
-import {
-  useAccount,
-  useWaitForTransactionReceipt,
-  useWriteContract,
-} from "wagmi";
+import { useAccount, usePublicClient, useWriteContract } from "wagmi";
 import { subnetABI as abi } from "@/abis";
 import { useTransactionDialog } from "@/components/transaction-provider";
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { subnetContractAddress } from "@/constants/contracts";
 
 export const useRegisterMinerInfo = () => {
   const queryClient = useQueryClient();
+  const publicClient = usePublicClient();
 
   const { address } = useAccount();
 
-  const { data: hash, writeContractAsync } = useWriteContract();
+  const { writeContractAsync } = useWriteContract();
 
   const { showDialog, hideDialog } = useTransactionDialog();
-
-  const {
-    isLoading: isConfirming,
-    isSuccess,
-    isError,
-    error,
-    data: transaction,
-  } = useWaitForTransactionReceipt({
-    hash,
-  });
-
-  console.log(isConfirming, isSuccess, isError, transaction);
 
   const registerMinerInfo = useCallback(async () => {
     console.log("registerMinerInfo");
@@ -40,58 +25,53 @@ export const useRegisterMinerInfo = () => {
     });
 
     try {
-      await writeContractAsync({
+      const hash = await writeContractAsync({
         abi,
         address: subnetContractAddress,
         functionName: "registerMinerInfo",
         args: [address!],
       });
-
       showDialog({
         title: "Transaction Confirmation",
         content: "Transaction Pending",
         status: "loading",
       });
-    } catch (e) {
-      showDialog({
-        title: "Transaction Error",
-        content: "Please try again",
-        status: "error",
-      });
-      console.log(e);
-    }
-  }, [address, showDialog, writeContractAsync]);
 
-  useEffect(() => {
-    if (!isConfirming && transaction?.status === "success") {
+      const transaction = await publicClient?.waitForTransactionReceipt({
+        hash,
+      });
+
+      if (transaction?.status === "reverted") {
+        throw new Error("Transaction Reverted");
+      }
+
       showDialog({
         title: "Transaction Confirmation",
         content: "Transaction Confirmed",
         status: "success",
       });
-      setTimeout(() => {
-        queryClient.invalidateQueries({
-          queryKey: ["miner", address],
-        });
-        hideDialog();
-      }, 3000);
-    } else if (!isConfirming && transaction?.status === "reverted") {
+    } catch (e) {
+      console.error(e);
       showDialog({
         title: "Transaction Error",
-        content: "Transaction Reverted",
+        content: "Please try again",
         status: "error",
       });
-      setTimeout(() => {
-        hideDialog();
-      }, 1500);
     }
+
+    setTimeout(() => {
+      queryClient.invalidateQueries({
+        queryKey: ["miner", address],
+      });
+      hideDialog();
+    }, 3000);
   }, [
     address,
     hideDialog,
-    isConfirming,
+    publicClient,
     queryClient,
     showDialog,
-    transaction?.status,
+    writeContractAsync,
   ]);
 
   return { registerMinerInfo };
